8db94bced6fcf041eb63a50ea6538772736cd272
David OC <davidherzlos@gmail.com>
2021-04-14 16:00:33 -0500

    Chapter 19, set the table: Invoke set_up first

--- tests/test_testing_framework.py
+++ tests/test_testing_framework.py
@@ -2,29 +2,42 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def set_up(self):
+        pass
+
     def run(self):
+        self.set_up()
         getattr(self, self.name)()
 
 
 class WasRun(TestCase):
     def __init__(self, name):
         super().__init__(name)
-        self.was_run = None
 
     def test_method(self):
         self.was_run = 1
 
+    def set_up(self):
+        self.was_run = None
+        self.was_set_up = 1
+
 
 def main():
     class TestCaseTest(TestCase):
+        def set_up(self):
+            self.test = WasRun('test_method')
+
         def test_running(self):
-            test = WasRun('test_method')
-            assert not test.was_run
-            test.run()
-            assert test.was_run
+            self.test.run()
+            assert self.test.was_run
+
+        def test_set_up(self):
+            self.test.run()
+            assert self.test.was_set_up
 
     TestCaseTest('test_running').run()
+    TestCaseTest('test_set_up').run()
 
-    
+
 if __name__ == '__main__':
     main()
